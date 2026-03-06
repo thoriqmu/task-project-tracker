b171db014971f3e1a7e9dde6aaab7eca9df9b68b
Thoriq Muchlishin <tmuchlishin@gmail.com>
2026-03-06 21:44:32 +0700

    Install vue.js 3 dan konfigurasi vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/main.ts'],
             refresh: true,
         }),
+        vue(),
         tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'resources/js'),
         },
     },
 });
